15fb09bcf896ddfd0030b394814d0e547673a76f
Harsh Popat <harshpopat75@gmail.com>
2021-10-10 00:53:06 +0530

    mongodb connected

--- config/db.js
+++ config/db.js
@@ -2,28 +2,20 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-// const connectDB = () => {
-//   mongoose.connect(db);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+//   mongoose.connect(db, {
+//     serverSelectionTimeoutMS: 5000,
+//   });
 // };
+// .catch((err) => console.log(err.reason));
 
-// Prints "MongoServerError: bad auth Authentication failed."
-mongoose
-  .connect(db, {
-    serverSelectionTimeoutMS: 5000,
-  })
-  .catch((err) => console.log(err.reason));
-
-// main().catch((err) => console.log(err));
-
-// async function main() {
-//   await mongoose.connect(db);
-// }
-
-//     console.log('MongoDB Connected...');
-//   } catch (err) {
-//     console.error(err.message);
-//     process.exit(1);
-//   }
-// };
-
-exports.connectDb = connectDb;
+module.exports = connectDB;
