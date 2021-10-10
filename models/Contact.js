8d252c73b4a3d1da34f54084c6c05b76437a7b44
Harsh Popat <harshpopat75@gmail.com>
2021-10-10 16:47:38 +0530

    contact model and get contacts route

---
+++ models/Contact.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Contactschema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: 'personal',
+  },
+  date: {
+    type: String,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('contact', Contactschema);
